ca74603f23bc7e0dad31fccc582b9bd17bcaac23
Evan Shrestha <Evanshrestha@gmail.com>
2018-10-18 21:19:06 -0500

    - Connected views with segues

--- group3_assignment6/AdventurersViewController.swift
+++ group3_assignment6/AdventurersViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class AdventurersViewController: UIViewController {
+class AdventurersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "adventurerCell")
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
